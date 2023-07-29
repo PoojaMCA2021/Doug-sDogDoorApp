809a6f31a9bacd29eea4fa5956cf07213448ec09
Isha <isha23229@gmail.com>
2023-07-29 23:31:30 +0530

    Add project files.

---
+++ Model/Remote.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace DogDoorApp.Model
+{
+    internal class Remote
+    {
+     
+        private DogDoor _door;
+        static Timer Timer;
+        public Remote(DogDoor door) 
+        { 
+            _door = door;
+        }
+        public void PressButton()
+        {
+            Console.WriteLine("Pressing the remote control button....");
+            if(_door.IsOpen())
+            {
+                _door.Close();
+            }
+            else {
+                _door.Open();
+
+
+               
+             
+            }
+        }
+
+        public void AutoClose()
+        {
+            Thread.Sleep(1000);
+            _door.Close();  
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using DogDoorApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace Doug_sDogDoorApp
+{
+    internal class Program
+    {
+
+        static Timer Timer;
+        static void Main(string[] args)
+        {
+
+            DogDoor door = new DogDoor();
+            Remote remote = new Remote(door);
+            Console.WriteLine("Fido barks to go outside....");
+            remote.PressButton();
+            Console.WriteLine("Fido has gone outside....");
+         
+            Console.WriteLine("Fido is all done....");
+          
+            Console.WriteLine("Fido is back inside....");
+            remote.AutoClose();
+
+
+        }
+      
+    }
+}
